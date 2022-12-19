2e7ea5bca2a09498dea063cfdc5c1e2570a729b0
HyunggiPark <phg9587@naver.com>
2022-12-19 19:51:10 +0900

    feat: busy-waiting 을 sleep-wake로 변경 (Header 파일 추가)

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -134,10 +134,11 @@ const char *thread_name (void);
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
 
-void thread_sleep(void);
-void thread_awake(void);
+void thread_sleep(int64_t ticks);
+void thread_awake(int64_t ticks);
 
-void update_next_global_tick (void);
+void update_next_global_tick (int64_t ticks);
+int64_t get_next_tick_to_awake(void);
 
 int thread_get_priority (void);
 void thread_set_priority (int);
