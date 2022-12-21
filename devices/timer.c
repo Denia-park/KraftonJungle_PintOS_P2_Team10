cb84ec45bca6b6516030a31c8811e9965c6232ba
HyunggiPark <phg9587@naver.com>
2022-12-21 18:23:23 +0900

    fix: timer_sleep 에 필요없는 부분 코드 삭제

--- devices/timer.c
+++ devices/timer.c
@@ -97,8 +97,7 @@ timer_sleep (int64_t ticks) {
 	int64_t start = timer_ticks ();
 
 	ASSERT (intr_get_level () == INTR_ON);
-	while (timer_elapsed (start) < ticks)
-		thread_sleep(start + ticks);
+	thread_sleep(start + ticks);
 }
 
 /* Suspends execution for approximately MS milliseconds. */
